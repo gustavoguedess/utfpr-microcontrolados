1dd8161f033c236a6ee02a962ef664c3642e6c7a
Franziska Furtado <franziskafurtado@gmail.com>
2022-09-05 23:57:36 -0300

    Update main.s
    upandos só essa file pra não bugar, acho que ta certo, coloquei o fluxograma no commit anterior

--- AP0/main.s
+++ AP0/main.s
@@ -29,8 +29,86 @@ saving
 	B saving
 
 primes
-	;Fazer os primos aqui [Franziska]
-	MOV R10,R0
+
+; *****************************************
+; * 		    Primos  				  *
+; *****************************************
+
+	CarregaListaNumeros
+	LDR R0, =Aleatorios
+	
+	MOV R1, #65
+	STRB R1, [R0], #1
+	MOV R1, #197
+	STRB R1, [R0], #1
+	MOV R1, #141
+	STRB R1, [R0], #1
+	MOV R1, #173
+	STRB R1, [R0], #1
+	MOV R1, #37
+	STRB R1,[R0],  #1
+	MOV R1, #129
+	STRB R1, [R0], #1
+	MOV R1, #211
+	STRB R1, [R0], #1
+	MOV R1, #5
+	STRB R1, [R0], #1
+	MOV R1, #207
+	STRB R1, [R0], #1
+	MOV R1, #3
+	STRB R1, [R0], #1
+	MOV R1, #23
+	STRB R1, [R0], #1
+	MOV R1, #17
+	STRB R1, [R0], #1
+	MOV R1, #209
+	STRB R1, [R0], #1
+	MOV R1, #243
+	STRB R1, [R0], #1
+	MOV R1, #179
+	STRB R1, [R0], #1
+	MOV R1, #241
+	STRB R1, [R0], #1 
+	MOV R1, #237
+	STRB R1, [R0], #1
+	MOV R1, #107
+	STRB R1, [R0], #1
+	MOV R1, #111
+	STRB R1, [R0], #1
+	MOV R1, #21
+	STRB R1, [R0], #1
+
+	; conferir a numeracao dos registradores
+	LDR R1, =Aleatorios ;Registrador read
+	LDR R2, =Primos ;Registrador write
+	MOV R4, #0 ;comprimento da lista de primos
+
+TestaSePrimo
+	CMP R0, R1
+	BEQ FimPrimos ;Se posições forem iguais, fim da lista
+	
+	LDRB R12, [R1], #1 ;Coloca número a ser testado no registrador *R12*
+	
+	CMP R12, #1
+	BEQ CopiaPrimo ;Se número igual a 1, copia o primo
+	
+	MOV R3, #2 ;Coloca primeiro divisor a ser testado no *R3*
+Divisao
+	CMP R12, R3
+	BEQ CopiaPrimo ;se numero for igual ao divisor atual, copia 
+	UDIV R11, R12, R3
+	MLS R11, R11, R3, R12
+	CMP R11, #0
+	BEQ TestaSePrimo ;se resto da divisão = 0, passa para o próximo
+	ADD R3, #1
+	B Divisao
+	
+CopiaPrimo
+	STRB R12, [R2], #1 ;copia número primo para a memória
+	ADD R4, #1 ;atualiza lista de primos
+	B TestaSePrimo
+
+FimPrimos
 	
 ; *****************************************
 ; * 			Bubblesort				  *
